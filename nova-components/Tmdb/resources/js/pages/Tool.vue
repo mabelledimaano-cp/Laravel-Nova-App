61dd028c8548e0c19bee0b9c598360183c016a04
mabelledimaano-cp <mabelle.dimaano@coreproc.com>
2023-11-20 13:37:27 +0800

    stylized the custom tool (TMDB)

--- nova-components/Tmdb/resources/js/pages/Tool.vue
+++ nova-components/Tmdb/resources/js/pages/Tool.vue
@@ -7,17 +7,22 @@
         <Card
             class="mb-6 movie-container"
         >
-            <div v-for="movie in data.items" :key="movie.id" class="movie-card">
-                <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="">
-                <h2>{{ movie.title }}</h2>
-                <p>{{ movie.overview }}</p>
-                <span>Rating: </span>
-                <span v-html="generateStarRating(movie.vote_average)"></span>
-                <span> ({{ (movie.vote_average * 10).toFixed(2) }}%)</span>
-                <p> Release Date: {{ moment(movie.release_date).format('MMMM DD, YYYY') }}</p>
-
-                <!-- Add other movie details as needed -->
+            <div v-for="movie in data.items" :key="movie.id" class="movie-card flex flex-col justify-between">
+                <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="" class="w-15">
+                <div class="flex flex-col justify-between h-full">
+                    <div>
+                        <h2 class="text-xl font-bold mb-2">{{ movie.title }}</h2>
+                        <p class="max-w-2xl text-justify mb-2">{{ movie.overview }}</p>
+                    </div>
+                    <div class="mt-auto">
+                        <span>Rating: </span>
+                        <span v-html="generateStarRating(movie.vote_average)"></span>
+                        <span> ({{ (movie.vote_average * 10).toFixed(2) }}%)</span>
+                        <p>Release Date: {{ moment(movie.release_date).format('MMMM DD, YYYY') }}</p>
+                    </div>
+                </div>
             </div>
+
         </Card>
     </div>
 </template>
@@ -32,20 +37,17 @@ const data = reactive({
 })
 
 const generateStarRating = (rating) => {
-    // Scale the rating from 0-10 to the 0-5 star system
+
     let roundedRating = Math.round(rating / 2 + (.45))
 
-    // Ensure the roundedRating is within the valid range of 0 to 5
     roundedRating = Math.min(Math.max(roundedRating, 0), 5)
 
     let starIcons = '★'.repeat(Math.floor(roundedRating))
 
     if (roundedRating % 1 !== 0) {
-        // If there's a decimal part, add a half star
         starIcons += '<span class="half-star">½</span>'
     }
 
-    // Add empty stars for the remaining space
     starIcons += '☆'.repeat(5 - Math.ceil(roundedRating))
 
     return starIcons
@@ -61,7 +63,7 @@ onMounted(() => {
 
 <style scoped>
 .half-star {
-    font-size: 0.8em; /* Adjust the font size as needed */
+    font-size: 0.8em;
 }
 
 .movie-container {
@@ -71,8 +73,12 @@ onMounted(() => {
 }
 
 .movie-card {
-    width: calc(25% - 16px); /* Adjust the width based on your design, considering margins */
-    margin-bottom: 16px; /* Add margin for spacing between cards */
+    width: calc(25% - 16px);
+    margin-bottom: 16px;
+}
+
+.text-justify {
+    text-align: justify;
 }
 
 </style>
